58adbe98647f19c4302b6f93eecd9ad3d24c5dab
Kim Se Yong <seyongski8@gmail.com>
2024-04-02 17:49:39 +0900

    Added Japanese compatibility

--- simple_chatbot.py
+++ simple_chatbot.py
@@ -2,12 +2,12 @@ import json
 from difflib import get_close_matches
 
 def load_knowledge_base(file_path: str) -> dict:
-  with open(file_path, 'r') as file:
+  with open(file_path, 'r', encoding='utf-8') as file:
     data: dict = json.load(file)
   return data
 
 def save_knowledge_base(file_path: str, data: dict):
-  with open(file_path, 'w') as file:
+  with open(file_path, 'w', encoding='utf-8') as file:
     json.dump(data, file, indent=2)
 
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
@@ -35,13 +35,13 @@ def chat_bot():
       answer: str = get_answer_for_question(best_match, knowledge_base)
       print(f'Bot: {answer}')
     else:
-      print("Bot: I don't know the answer. Can you teach me?")
-      new_answer: str = input('Type the answer or "skip" to skip: ')
+      print("Bot: 答えない、教えて下さい。")
+      new_answer: str = input('答えを入力するか、「skip」でスキップする: ')
 
       if new_answer.lower() != 'skip':
         knowledge_base['questions'].append({'question': user_input, 'answer': new_answer})
         save_knowledge_base('knowledge_base.json', knowledge_base)
-        print('Bot: Thank you! I learned a new response!')
+        print('Bot: ありがとう！新しい答えをまなんでた！')
 
 if __name__ == '__main__':
   chat_bot()
